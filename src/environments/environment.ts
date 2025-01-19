4ed25725354ffa98fab9b94db71ab552a8fdb063
Lucas.meireles <lucasgnam12@gmail.com>
2025-01-19 00:36:43 -0300

    chore: initial structure

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiUrl: '',
+    // keycloak: {
+    //   ssoUrl: 'https://sso.stg.cloud.pje.jus.br/auth/',
+    //   realm: 'pje',
+    //   clientId: 'sispreq-frontend',
+    // },
+  };
+  
